fd355667fe719534cac3352284a3ff75f4497342
염태 <@DESKTOP-7QP0C4V>
2023-01-27 10:27:37 +0900

    1.프로젝트생성

---
+++ Jspgit/src/main/java/_1/Index.java
@@ -0,0 +1,25 @@
+package _1;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/index")
+public class Index extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Index() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("NewFile.jsp");
+		dispatcher.forward(request, response);
+	}
+}
---
+++ Jspgit/src/main/webapp/NewFile.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Insert title here</title>
+</head>
+<body>
+  1. 처음으로 만든 파일
+</body>
+</html>
